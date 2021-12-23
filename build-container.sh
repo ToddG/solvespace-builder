146e5018cea0d65709b2ee9107331ae9b5a8e005
Todd Greenwood-Geer <toddg@envirosoftwaresolutions.com>
2021-12-23 13:40:21 -0800

    still messing with submodules

--- build-container.sh
+++ build-container.sh
@@ -3,17 +3,22 @@
 # sigh. b/c cmake fails to get the git commit hash from a submodule.
 
 # this does not work... I did this and then had to 'deinit' the submodule
-# git submodule add https://github.com/solvespace/solvespace solvespace-source
+git submodule add https://github.com/solvespace/solvespace solvespace-source
 
-# so here we manually clone if it doesn't already exist
-if [[ ! -d solvespace-source ]]
-then
-    git clone https://github.com/solvespace/solvespace solvespace-source
-fi
+## so here we manually clone if it doesn't already exist
+#if [[ ! -d solvespace-source ]]
+#then
+#    git clone https://github.com/solvespace/solvespace solvespace-source
+#fi
 
 pushd solvespace-source
 git submodule update --init --recursive
 popd
+
+if [[ ! -d solvespace-bin ]]
+then
+    mkdir solvespace-bin
+fi
 
 # use this if things get wonky
 #docker build docker/. -t solvespace-container:latest --no-cache
